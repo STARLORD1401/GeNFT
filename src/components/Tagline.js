f6ee1e41b37c100397ac9c3f977d8a8e9d5db063
STARLORD1401 <shreyaspjoshi.14@gmail.com>
2022-09-18 13:09:20 +0530

    Added tagline.js

---
+++ src/components/Tagline.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Tagline() {
+  return (
+    <div className="bg-dark rounded-pill p-3">
+      <div className="d-flex">
+        <li className="text-primary">GENERATE</li>
+        <li className="text-secondary mx-3">HODL</li>
+        <li className="text-tertiary">SELL</li>
+      </div>
+    </div>
+  );
+}
+
+export default Tagline;
